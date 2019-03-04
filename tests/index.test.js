14232c78534d7a5b405ef1dfbc536ee37256fb18
WebMats <lorlando127@gmail.com>
2019-03-04 17:10:15 -0500

    added file structure and basic logic

---
+++ tests/index.test.js
@@ -0,0 +1,19 @@
+const { success, fail, repair } = require('../enhancer');
+const getItem = require('../build-item');
+
+let item;
+
+beforeEach(() => {
+    item = getItem();
+})
+
+describe('when enhancement succeeds', () => {
+    
+  
+})
+
+
+
+describe('when enhancement fails', () => {
+  
+})
