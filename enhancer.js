64a730abd80ac2fc10f7985b21c70f59b2fb9691
WebMats <lorlando127@gmail.com>
2019-03-04 17:39:39 -0500

    testing that durability is decreased by 10

--- enhancer.js
+++ enhancer.js
@@ -3,6 +3,10 @@ const success = (item) => {
 }
 
 const fail = (item) => {
+    if (item.enhancement > 14) {
+        const updatedItem = {...item, durability: item.durability - 10}
+        return updatedItem.durability
+    }
     const updatedItem = {...item, durability: item.durability - 5}
     return updatedItem.durability
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -15,11 +15,24 @@ describe('when enhancement fails', () => {
     beforeAll(() => {
         enhance = fail;
     })
-    describe('when enhancement is between 0 and 14.', () => {
-        test('should durability decrease by 5', () => {
+    describe('when enhancement is between 0 and 14', () => {
+        test('should decrease durability by 5.', () => {
             const durabilityBefore = item.durability;
             const durabilityAfter = enhance(item);
             expect(durabilityAfter).toEqual(durabilityBefore - 5);
+        })
+    })
+    describe('when enhancement is greater than 14', () => {
+        beforeAll(() => {
+            const difference = 15 - item.enhancement;
+            if (difference > 0) {
+                item.enhancement = item.enhancement + difference
+            }
+        })
+        test('should decrease durability by 10.', () => {
+            const durabilityBefore = item.durability;
+            const durabilityAfter = enhance(item);
+            expect(durabilityAfter).toEqual(durabilityBefore - 10);
         })
         
     })
